d567a012b34cdc4b6dccf20a6faf3928cadbaa50
Marcelo Tavora <marcelovtneves@gmail.com>
2024-03-07 21:56:27 -0300

    Feito primeira estrutura do menu

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,7 +1,12 @@
+import { SidebarMenuComponent } from '@pages/Home/components/SidebarMenu'
+
 export const HomePage = () => {
   return (
-    <div className="flex justify-content-center align-items-center">
-      <h1>Bem-Vindo ao OmniChat</h1>
+    <div className="flex">
+      <SidebarMenuComponent />
+      <div className={'bg-primary flex justify-content-center'} style={{ width: 'calc(100vw - 280px)' }}>
+        <h1>Bem-Vindo ao OmniChat</h1>
+      </div>
     </div>
   )
 }
