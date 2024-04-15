ce141be814356d23aeb7caa72cc568a5072ac62a
Marcelo Tavora <marcelovtneves@gmail.com>
2024-04-15 15:06:27 -0300

    💄  Ajustes visuais no footer da sidebar

--- src/pages/Home/components/SidebarHome/SidebarFooterComponent.tsx
+++ src/pages/Home/components/SidebarHome/SidebarFooterComponent.tsx
@@ -9,6 +9,7 @@ export const SidebarFooterComponent = ({ isTopbarFooter }: SidebarFooterProps) =
       <hr className="mb-3 mx-3 border-top-1 border-none surface-border" />
       <a className="m-3 flex align-items-center cursor-pointer p-3 gap-2 border-round text-700 hover:surface-100 transition-duration-150 transition-colors p-ripple">
         <div
+          className="text-center"
           style={{
             width: !isTopbarFooter ? '200px' : '400px',
             overflowX: 'hidden',
