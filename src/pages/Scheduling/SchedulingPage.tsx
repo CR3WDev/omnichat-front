c55a0f47d70c4a3df837b2bbddef1dc60342d052
Davi Tavares Frota <73137633+Davitfrota@users.noreply.github.com>
2024-05-06 08:12:12 -0300

    Tela de Calendario
    Tela de calendario antes do pente fino

--- src/pages/Scheduling/SchedulingPage.tsx
+++ src/pages/Scheduling/SchedulingPage.tsx
@@ -1,60 +1,204 @@
-import { Button } from 'primereact/button'
-import { Calendar } from 'primereact/calendar'
-import { Divider } from 'primereact/divider'
-import { Dropdown } from 'primereact/dropdown'
-import { useState } from 'react'
-import { SchedulingConfirmDialog } from './components/SchedulingConfirmDialog'
+import { Button } from 'primereact/button';
+import { Calendar } from 'primereact/calendar';
+import { Checkbox } from 'primereact/checkbox';
+import { Divider } from 'primereact/divider';
+import { Dropdown } from 'primereact/dropdown';
+import { MultiSelect } from 'primereact/multiselect';
+import { useEffect, useState } from 'react';
+import { SchedulingConfirmDialog } from './components/SchedulingConfirmDialog';
+
+interface Barber {
+  name: string;
+  services: string[];
+  workingHours: { [key: string]: { start: string; end: string } }; // Horários de trabalho para cada dia da semana
+}
 
 export const SchedulingPage = () => {
-  const [date, setDate] = useState(null)
-  const schedulingList = ['10:00', '10:30', '11:30']
-  const [isVisible, setIsVisible] = useState(false)
+  const [date, setDate] = useState<Date | null>(null);
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const [selectedBarber, setSelectedBarber] = useState<Barber | null>(null);
+  const [selectedService, setSelectedService] = useState<string | null>(null);
+  const [checked, setChecked] = useState<boolean>(false);
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const [selectedTime, setSelectedTime] = useState<string | null>(null);
+  const [availableTimes, setAvailableTimes] = useState<string[]>([]);
+  const [bookedTimes, setBookedTimes] = useState<string[]>([]); // Lista de horários marcados
+
+  const barbers: Barber[] = [
+    {
+      name: 'junin',
+      services: ['Corte', 'Barba'],
+      workingHours: {
+        Monday: { start: '08:00', end: '16:00' },
+        Tuesday: { start: '08:30', end: '17:00' },
+        Wednesday: { start: '09:00', end: '16:30' },
+        Thursday: { start: '08:00', end: '16:00' },
+        Friday: { start: '08:30', end: '17:00' },
+        Saturday: { start: '10:00', end: '15:30' },
+        Sunday: { start: '', end: '' }, // Domingo não trabalha
+      },
+    },
+    {
+      name: 'marcelo',
+      services: ['Corte', 'Sobrancelha'],
+      workingHours: {
+        Monday: { start: '09:00', end: '17:00' },
+        Tuesday: { start: '09:30', end: '17:30' },
+        Wednesday: { start: '08:00', end: '15:30' },
+        Thursday: { start: '09:00', end: '17:00' },
+        Friday: { start: '09:30', end: '17:30' },
+        Saturday: { start: '10:00', end: '15:30' },
+        Sunday: { start: '', end: '' }, // Domingo não trabalha
+      },
+    },
+  ];
+
+  useEffect(() => {
+    if (date && selectedBarber) {
+      const dayOfWeek = date.toLocaleString('en-US', { weekday: 'long' });
+      const workingHours = selectedBarber.workingHours[dayOfWeek];
+      if (workingHours.start && workingHours.end) {
+        const startTime = new Date(date);
+        startTime.setHours(Number(workingHours.start.split(':')[0]), Number(workingHours.start.split(':')[1]));
+
+        const endTime = new Date(date);
+        endTime.setHours(Number(workingHours.end.split(':')[0]), Number(workingHours.end.split(':')[1]));
+
+        const availableTimesForDay: string[] = [];
+
+        while (startTime < endTime) {
+          availableTimesForDay.push(`${String(startTime.getHours()).padStart(2, '0')}:${String(startTime.getMinutes()).padStart(2, '0')}`);
+          startTime.setMinutes(startTime.getMinutes() + 30); // Adiciona 30 minutos ao horário
+        }
+
+        setAvailableTimes(availableTimesForDay);
+      } else {
+        setAvailableTimes([]);
+      }
+    }
+  }, [date, selectedBarber]);
+
+  const handleServiceChange = (e: any) => {
+    const service = e.value;
+    setSelectedService(service);
+  };
+
+  const handleTimeClick = (time: string) => {
+    setSelectedTime(time);
+    setIsVisible(true);
+  };
+
+  const handleDateClick = (event: any) => {
+    setDate(event.value);
+    setSelectedDate(event.value);
+  };
+
+  const handleConfirmDialogClose = (confirmed: boolean) => {
+    setIsVisible(false);
+    if (confirmed) {
+      // implementar a lógica para salvar o agendamento
+      if (selectedBarber && selectedService && selectedDate && selectedTime) {
+        const confirmationDetails = `Barbeiro - ${selectedBarber.name}, Serviço - ${selectedService}, Data - ${selectedDate}, Horário - ${selectedTime}`;
+        console.log("Agendamento confirmado:", confirmationDetails);
+
+        // Adiciona o horário marcado à lista de horários marcados
+        setBookedTimes(prevTimes => [...prevTimes, `${selectedDate.toDateString()} ${selectedTime}`]);
+      }
+    }
+  };
+
   return (
     <>
-      <div className="text-center">
+      <div className="text-center flex w-screen h-screen flex-column justify-content-start align-items-center">
         <h1>Agendamento</h1>
-        {/* <Card>
-        <h2>Raveth - Barbearia</h2>
-      </Card> */}
-        <Divider></Divider>
-        <div className="flex">
-          <Dropdown options={['junin', 'marcelo']}></Dropdown>
-          <div className="card flex justify-content-center mr-2">
-            <Calendar
-              value={date}
-              onChange={(e: any) => setDate(e.value)}
-              inline
-              showWeek
-              panelStyle={{ border: 0 }}
-            />
-          </div>
-          <Divider layout="vertical" />
-          <div>
-            <div>
-              <h4>Horários</h4>
+        <div>
+          <Divider></Divider>
+          <div className="flex">
+            <div id='menuBarbeiro' className='flex flex-column'>
+              <div>
+                <h2>Barbearia Carecas Unidos</h2>
+              </div>
+              <div className='flex w-full flex-column'>
+                <h3>Barbeiros</h3>
+                <Dropdown
+                  placeholder='Selecione um barbeiro'
+                  options={barbers.map(barber => barber.name)}
+                  value={selectedBarber ? selectedBarber.name : null}
+                  onChange={(e) => {
+                    const selectedBarberName = e.value;
+                    const barber = barbers.find(barber => barber.name === selectedBarberName);
+                    setSelectedBarber(barber || null);
+                    setSelectedService(null);
+                  }}
+                ></Dropdown>
+              </div>
+              <div className='flex w-full flex-column'>
+                <h3>Serviço</h3>
+                <MultiSelect
+                  placeholder='Selecione um serviço'
+                  options={selectedBarber ? selectedBarber.services : []}
+                  value={selectedService}
+                  onChange={handleServiceChange}
+                  display="chip"
+                  disabled={!selectedBarber}
+                  selectAllLabel="Selecionar Todos"
+                ></MultiSelect>
+              </div>
+              <div className='flex align-items-center w-full flex-row'>
+                <Checkbox style={{ transform: 'scale(1.25)' }} onChange={(e) => setChecked(e.checked ?? false)} checked={checked}>Serviço</Checkbox>
+                <h4 className="ml-2">Mostrar Apenas os Horários Disponíveis </h4>
+              </div>
             </div>
-            <div className="flex flex-column">
-              {schedulingList.map((scheduling: string, index: number) => {
-                return (
-                  <div className="mb-2">
-                    <Button
-                      label={scheduling}
-                      severity="secondary"
-                      onClick={() => {
-                        setIsVisible(true)
-                      }}
-                      text
-                      raised
-                      style={{ width: '300px' }}
-                    />
-                  </div>
-                )
-              })}
+            <Divider layout='vertical'></Divider>
+            {selectedBarber && selectedService ? (
+              <>
+                <div>
+                  <Calendar
+                    value={date}
+                    onChange={handleDateClick}
+                    inline
+                    locale='pt'
+                    minDate={new Date()}
+                    maxDate={new Date(new Date().setMonth(new Date().getMonth() + 3))} // Define a data máxima como três meses no futuro
+                    yearNavigator={false}
+                    yearRange={'2022:2025'}
+                    panelStyle={{ border: 0 }}
+                    style={{ maxHeight: '480px' }}
+                  />
+                </div>
+              </>
+              ) : (
+            <div className="card flex justify-content-center mr-2">
+              <h2 className="text-center">Por favor, selecione um barbeiro e o serviço.</h2>
+            </div>
+          )}
+            <Divider layout="vertical" />
+            <div>
+              <div>
+                <h2>Horários Disponíveis</h2>
+              </div>
+              <div className="flex flex-wrap" style={{ width: '300px' }}>
+                {availableTimes.map((time, index) => (
+                  (!checked || !bookedTimes.includes(`${selectedDate?.toDateString()} ${time}`)) && (
+                    <div className="mb-2 ml-2" key={index}>
+                      <Button
+                        label={time}
+                        severity="secondary"
+                        onClick={() => handleTimeClick(time)}
+                        text
+                        raised
+                        style={{ width: '140px' }}
+                        disabled={bookedTimes.includes(`${selectedDate?.toDateString()} ${time}`)}
+                      />
+                    </div>
+                  )
+                ))}
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <SchedulingConfirmDialog setIsVisible={setIsVisible} isVisible={isVisible} />
+      <SchedulingConfirmDialog setIsVisible={setIsVisible} isVisible={isVisible} onClose={handleConfirmDialogClose} selectedBarber={selectedBarber} selectedService={selectedService} selectedTime={selectedTime} barbers={barbers} selectedDate={selectedDate} />
     </>
-  )
-}
+  );
+};
--- src/pages/Scheduling/components/SchedulingConfirmDialog.tsx
+++ src/pages/Scheduling/components/SchedulingConfirmDialog.tsx
@@ -1,15 +1,87 @@
-import { Dialog } from 'primereact/dialog'
+import { Dialog } from 'primereact/dialog';
+import { Dropdown } from 'primereact/dropdown';
+import { MultiSelect } from 'primereact/multiselect';
+import { useState } from 'react';
 
-export const SchedulingConfirmDialog = ({ isVisible, setIsVisible }) => {
+interface Props {
+  isVisible: boolean;
+  setIsVisible: (isVisible: boolean) => void;
+  onClose: (confirmed: boolean) => void;
+  selectedBarber: Barber | null;
+  selectedService: string | null;
+  selectedTime: string | null;
+  selectedDate: Date | null;
+  barbers: Barber[];
+}
+
+interface Barber {
+  name: string;
+  services: string[];
+  workingHours: { [key: string]: { start: string; end: string } };
+}
+
+export const SchedulingConfirmDialog: React.FC<Props> = ({ isVisible, setIsVisible, onClose, selectedBarber, selectedService, selectedTime, selectedDate, barbers }: Props) => {
+  const [selectedServices, setSelectedServices] = useState<string[]>(selectedService ? [selectedService] : []);
+
+  const handleServiceChange = (e: any) => {
+    const services = e.value;
+    setSelectedServices(services);
+  };
+
+  // Função para formatar a data como string no formato desejado
+  const formatDate = (date: Date | null) => {
+    return date ? new Date(date).toLocaleDateString('pt-BR') : ''; // Formata a data como string no formato dd/mm/aaaa
+  };
+
   return (
     <Dialog
       visible={isVisible}
-      draggable={false}
       onHide={() => {
-        setIsVisible(false)
+        setIsVisible(false);
+        onClose(false);
       }}
+      header="Confirmação de Agendamento"
+      modal
+      draggable={false}
+      style={{ width: '400px' }}
+      footer={
+        <div>
+          <button className="p-button p-button-text" onClick={() => onClose(false)}>Cancelar</button>
+          <button className="p-button p-button-primary" onClick={() => onClose(true)}>Confirmar</button>
+        </div>
+      }
     >
-      oi
+      <div id='menuBarbeiro' className='flex flex-column'>
+        <div>
+          {/* Utiliza a função formatDate para exibir a data formatada */}
+          <h3>Horário selecionado com {selectedBarber?.name} às {selectedTime} no dia {formatDate(selectedDate)}</h3>
+        </div>
+        <div className='flex w-full flex-column'>
+          <h3>Barbeiros</h3>
+          <Dropdown
+            placeholder='Selecione um barbeiro'
+            options={barbers.map(barber => barber.name)}
+            value={selectedBarber ? selectedBarber.name : null}
+            disabled
+            onChange={(e) => {
+              const selectedBarberName = e.value;
+              const barber = barbers.find(barber => barber.name === selectedBarberName);
+            }}
+          ></Dropdown>
+        </div>
+        <div className='flex w-full flex-column'>
+          <h3>Serviço</h3>
+          <MultiSelect
+            placeholder='Selecione um serviço'
+            options={selectedBarber ? selectedBarber.services : []}
+            value={selectedService}
+            onChange={handleServiceChange}
+            display="chip"
+            disabled
+            selectAllLabel="Selecionar Todos"
+          ></MultiSelect>
+        </div>
+      </div>
     </Dialog>
-  )
-}
+  );
+};
